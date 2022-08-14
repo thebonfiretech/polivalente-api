8a6c34f67d16cdfa5c5b698a420bafaa91db89b9
DARKnx <joao.vitornl@gmail.com>
2022-08-13 21:05:24 -0300

    :tada: Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+
+const app = express();
+const PORT = 8080
+
+app.listen(PORT, () => console.log("Server online em http://localhost:8080"));
+
+app.get("/", (req,res) => res.sendStatus(200))
