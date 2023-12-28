b4a21a9d40439cadfba63f25f4ec64a8c9704b3a
Dutra PC <joao.vitornl@gmail.com>
2023-12-27 21:35:10 -0300

    :construction: Construction sac service functions

--- src/resources/sac/sac.service.js
+++ src/resources/sac/sac.service.js
@@ -4,21 +4,27 @@ export default class sacService {
 
     async createSac({author, type, description}){
         try {
-
+            const sac = new sacModel({
+                author, type, description
+            }, {new: true})
+            await sac.save();
+            return { sac }
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async getSac({id}){
         try {
-
+            const findSac = await sacModel.findById(id);
+            if (!findSac) return { error: "sac_not_found"}
+            return { sac: findSac }
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async getAllSac({}){
         try {
-
+             return await sacModel.find().sort({date:-1});
         } catch (err) {
             return { error: "internal_error" } ;
         }
