0333e3d62687b185ca874a4cbc76590534f322e1
Dutra PC <joao.vitornl@gmail.com>
2023-12-27 21:31:37 -0300

    :construction: Construction menu service functions

--- src/resources/menu/menu.service.js
+++ src/resources/menu/menu.service.js
@@ -4,14 +4,18 @@ export default class menuService {
 
     async getMenu({}){
         try {
-            
+            return await menuModel.findOne();
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async updateMenu({id, data}){
         try {
+            const findMenu = await menuModel.findById(id);
+            if (!findMenu) return { error: "menu_not_found"}
 
+            const newMenu = await menuModel.findByIdAndUpdate(id, {$set:{ ...data}},{new: true,setDefaultsOnInsert: True});
+            return { menu: newMenu }
         } catch (err) {
             return { error: "internal_error" } ;
         }
