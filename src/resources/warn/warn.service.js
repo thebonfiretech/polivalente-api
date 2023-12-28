1e8f57b3307617ed7af8eb1236c71ac0309cbee4
Dutra PC <joao.vitornl@gmail.com>
2023-12-27 21:41:44 -0300

    :construction: Construction warn service and functions

--- src/resources/warn/warn.service.js
+++ src/resources/warn/warn.service.js
@@ -4,35 +4,50 @@ export default class WarnService {
 
     async createWarn({author, type, description, title}){
         try {
-
+            const warn = new warnModel({
+                author, type, description, title
+            }, {new: true}) 
+            await warn.save();
+            return { warn }
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async updateWarn({id, data}){
         try {
+            const findWarn = await warnModel.findById(id);
+            if (!findWarn) return { error: "warn_not_found"};
 
+            const newWarn = await warnModel.findByIdAndUpdate(id, {$set: {...data}}, {new: true, upsert: true});
+            return { warn: newWarn }
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async deleteWarn({id}){
-        try {
+        try {   
+            const findWarn = await warnModel.findById(id);
+            if (!findWarn) return { error: "warn_not_found"};
 
+            await warnModel.findByIdAndDelete(id);
+            return {}
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async getWarn({id}){
         try {
+            const findWarn = await warnModel.findById(id);
+            if (!findWarn) return { error: "warn_not_found"};
 
+            return { warn: findWarn }
         } catch (err) {
             return { error: "internal_error" } ;
         }
     }
     async getAllWarns({}){
         try {
-
+            return await warnModel.find().sort({date: -1})
         } catch (err) {
             return { error: "internal_error" } ;
         }
